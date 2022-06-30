588acdf1ef507cb3b5eae93ad37413c2cc327bb6
Katherine Hambley <katherine.hambley@azblue.com>
2022-06-30 09:19:11 -0500

    Added initial project files

--- HelloWorldDemoApp/HelloWorldDemoApp/App.xaml.cs
+++ HelloWorldDemoApp/HelloWorldDemoApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace HelloWorldDemoApp
 {
@@ -15,6 +18,13 @@ namespace HelloWorldDemoApp
 
         protected override void OnStart()
         {
+            var appSecret = "56f68048-dfa5-4a61-a1ce-84549ac69f2b";
+
+            AppCenter.Start("android={56f68048-dfa5-4a61-a1ce-84549ac69f2b};" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here};" +
+                  "macos={Your macOS App secret here};",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
